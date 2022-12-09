98da91e4c7e5047b2dc7389548dbb6f3dceb44e6
shoqqan <tataev.shokan@gmail.com>
2022-12-09 12:10:45 +0600

    settings(logic is not working)

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {Sheet} from "@mui/joy";
 import Typography from "@mui/joy/Typography";
-import {Button, Link, TextField} from "@mui/material";
+import {Button, FormControlLabel, Link, Radio, RadioGroup, TextField} from "@mui/material";
 import {useFormik} from "formik";
 
 const Settings = () => {
     const formik = useFormik({
         initialValues: {
-            familyName: '',
-            email: '',
-            password: '',
-            secondPassword: '',
-            familyImage: ''
+            language: ""
         },
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
         },
     });
+    const handleRadioButtons = (e: ChangeEvent<HTMLInputElement>) => {
+        formik.values.language = e.currentTarget.value
+        console.log(formik.values.language)
+    }
     return (
         <form onSubmit={formik.handleSubmit}>
             <Sheet variant="outlined" sx={{
@@ -30,89 +30,61 @@ const Settings = () => {
                 gap: 2,
                 borderRadius: 'sm',
                 boxShadow: 'md',
+                color: "#FEFEFE"
             }}>
                 <div>
                     <Typography sx={{marginBottom: 2, fontWeight: 'bold', fontSize: 25}} level="h4" component="h1">
                         Settings
                     </Typography>
-                    <Typography level="body2" color={"neutral"}>General</Typography>
+                    <Typography level="body2" color={"neutral"}>Language</Typography>
                 </div>
-                <TextField
-                    value={formik.values.familyName}
-                    onChange={formik.handleChange}
-                    // html input attribute
-                    name="familyName"
-                    type="text"
-                    placeholder="f.e 'Tatayev's name"
-                    // pass down to FormLabel as children
-                    label="Family name"
-                    error={formik.touched.familyName && Boolean(formik.errors.familyName)}
-                    helperText={formik.touched.familyName && formik.errors.familyName}
-                />
-                <TextField
-                    // html input attribute
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    name="email"
-                    type="email"
-                    placeholder="johndoe@email.com"
-                    // pass down to FormLabel as children
-                    label="Email"
-                    error={formik.touched.email && Boolean(formik.errors.email)}
-                    helperText={formik.touched.email && formik.errors.email}
-                />
-                <TextField
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                    name="password"
-                    type="password"
-                    placeholder="password"
-                    label="Password"
-                    error={formik.touched.password && Boolean(formik.errors.password)}
-                    helperText={formik.touched.password && formik.errors.password}
-                />
-                <TextField
-                    value={formik.values.secondPassword}
-                    onChange={formik.handleChange}
-                    variant={'outlined'}
-                    name="secondPassword"
-                    type="password"
-                    placeholder="password"
-                    label="Enter your password again"
-                    error={formik.touched.secondPassword && Boolean(formik.errors.secondPassword)}
-                    helperText={formik.touched.secondPassword && formik.errors.secondPassword}
-                />
+                <RadioGroup
+                    aria-labelledby="demo-controlled-radio-buttons-group"
+                    name={'language'}
+                    value={formik.values.language}
+                    onChange={e => handleRadioButtons(e)}>
+                    <FormControlLabel checked={false} value={'kazakh'} control={<Radio name={"language"}/>} label="Қазақша"/>
+                    <FormControlLabel checked={true} value={'english'} control={<Radio/>} label="English"/>
+                    <FormControlLabel checked={false}  value={'russian'}control={<Radio/>} label="Русский"/>
+                </RadioGroup>
 
-                <TextField
-                    id="input-with-icon-textfield"
-                    label="Family Avatar *"
-                    variant="outlined"
+                <Typography level="body2" color={"neutral"}>Theme</Typography>
+                <RadioGroup
+                    aria-labelledby="demo-controlled-radio-buttons-group"
+                    name={'language'}
+                    value={formik.values.language}
+                    onChange={e => handleRadioButtons(e)}>
+                    <FormControlLabel checked={false} value={'kazakh'} control={<Radio/>} label="White"/>
+                    <FormControlLabel checked={true} value={'english'} control={<Radio/>} label="Dark"/>
+                </RadioGroup>
+                {/*<input*/}
+                {/*    type="radio"*/}
+                {/*    id="one"*/}
+                {/*    name="group"*/}
+                {/*    value="One"*/}
+                {/*    onChange={e => handleRadioButtons(e)}*/}
+                {/*    required*/}
+                {/*/>*/}
+                {/*<label htmlFor="one">One</label>*/}
+                {/*<br />*/}
 
-                    sx={{
-                        ".MuiOutlinedInput-root": {
-                            paddingTop: "1rem",
-                            flexDirection: "column"
-                        },
-                        img: {
-                            paddingRight: "1rem"
-                        }
-                    }}
-                    InputProps={{
-                        startAdornment: <img src="https://via.placeholder.com/180x150/200"/>
-                    }}
-                    placeholder="Enter image caption..."
-                />
-                <Button type={"submit"} disabled={false} sx={{mt: 1 /* margin top */}}>
-                    Sign up family!
-                </Button>
-                <Typography
-                    endDecorator={<Link href="/sign-in">Sign in</Link>}
-                    fontSize="sm"
-                    sx={{alignSelf: 'center'}}
-                >
-                    Already have an account?
+                {/*<input*/}
+                {/*    type="radio"*/}
+                {/*    id="two"*/}
+                {/*    name="group"*/}
+                {/*    value="Two"*/}
+                {/*    onChange={e => handleRadioButtons(e)}*/}
+                {/*/>*/}
+                {/*<label htmlFor="two">Two</label>*/}
 
-                </Typography>
+                {/*<Typography*/}
+                {/*    endDecorator={<Link href="/sign-in">Sign in</Link>}*/}
+                {/*    fontSize="sm"*/}
+                {/*    sx={{alignSelf: 'center'}}*/}
+                {/*>*/}
+                {/*    Already have an account?*/}
+
+                {/*</Typography>*/}
 
             </Sheet>
         </form>
